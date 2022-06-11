4d8aaa908cc655d146078b1a0dba6ed4436eb909
211099 <211099@ids.upchiapas.edu.mx>
2022-06-11 15:52:38 -0500

    data change in config

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ export const api = {
     port: process.env.API_PORT || 3000,
 };
 export const db = {
-    user: "postgres",
-    host: "localhost",
-    database: "CSDB",
-    password: "211099",
+    user: "ccdzuifnzwrrcj",
+    host: "ec2-52-72-99-110.compute-1.amazonaws.com",
+    database: "daieh26r7540di",
+    password: "c2b0f2e807479f63516c3b7122f538a1533c189670e36b9ffde69340a01f90e7",
     port: "5432",
-}; 
+    dialect: "postgres"
+};
